d5e4b185a8df13e0a5440ebc115c5a8212206ed5
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-05 22:41:40 -0300

    Implemented Lucas number method

--- src/main/java/com/udemy/app/LucasSequence.java
+++ src/main/java/com/udemy/app/LucasSequence.java
@@ -31,7 +31,10 @@ public class LucasSequence {
    * @return Lucas number
    */
   public int lucas(int n) {
-    throw new RuntimeException("Method unimplemented");
+    if (n == 0) return 2;
+    if (n == 1) return 1;
+    
+    return lucas(n - 1) + lucas(n - 2);
   }
 
   /**
