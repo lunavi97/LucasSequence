5b49e6e3c9c85a94d2d8db175ff97b478145c422
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-05 22:44:03 -0300

    Reduced code of Lucas number method

--- src/main/java/com/udemy/app/LucasSequence.java
+++ src/main/java/com/udemy/app/LucasSequence.java
@@ -31,8 +31,7 @@ public class LucasSequence {
    * @return Lucas number
    */
   public int lucas(int n) {
-    if (n == 0) return 2;
-    if (n == 1) return 1;
+    if (n <= 1) return 2 - n;
     
     return lucas(n - 1) + lucas(n - 2);
   }
